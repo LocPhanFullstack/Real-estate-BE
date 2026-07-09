57c8da07e307f99a32f3a93e99cd6ce6bbbc0f5d
LocPhanFS <locphan@busmap.vn>
2026-07-09 15:59:04 +0700

    feat: create property API

--- dist/src/controllers/managerController.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-export declare const getManager: (req: Request<{
-    cognitoId: string;
-}>, res: Response) => Promise<void>;
-export declare const createManager: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=managerController.d.ts.map
